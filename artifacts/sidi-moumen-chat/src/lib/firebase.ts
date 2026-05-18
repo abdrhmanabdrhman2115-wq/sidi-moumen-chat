cec1b4aa63dba08c7cbb5e05efaf303a97bb92c1
abdrhmanabdrhman2115-wq <abdrhmanabdrhman2115@gmail.com>
2026-05-18 02:13:41 +0100

    add artifacts/sidi-moumen-chat/src/lib/firebase.ts

---
+++ artifacts/sidi-moumen-chat/src/lib/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZP9Jdov0vSWttmF_vDlT64twJbfXjWog",
+  authDomain: "sidi-moumen-74730.firebaseapp.com",
+  projectId: "sidi-moumen-74730",
+  storageBucket: "sidi-moumen-74730.firebasestorage.app",
+  messagingSenderId: "908225254735",
+  appId: "1:908225254735:web:fe3983209d53ac6a68a13a",
+  measurementId: "G-KS8RC33BJV",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
